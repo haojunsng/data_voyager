2d0c352440dbed3db79da76e5dfcf4d1a58e82b6
Sng Hao Jun <51106107+haojunsng@users.noreply.github.com>
2024-04-14 15:37:10 +0800

    SNG-13 feat: Use Github OIDC instead of service user (#24)
    * SNG-13 feat: Use Github OIDC instead of service user
    
    * SNG-13 feat: Grant permissions req to interact with GH OIDC Token Endpoint
    
    * SNG-13 fix: Point to Dockerfile instead of directory
    
    * SNG-13 debug
    
    * Revert "SNG-13 debug"
    
    This reverts commit 7df74febd9a6bdf72ff2ee7e6309a375aa6e92ad.
    
    * SNG-13 feat: Revert to initial docker build
    
    * SNG-13 feat: Delete gha service user

---
+++ pipeline/pipeline/terraform/github_oidc.tf
@@ -0,0 +1,57 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:haojunsng/strava_pipeline:*"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "github_actions" {
+  statement {
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+    ]
+    resources = [aws_ecr_repository.gomu_repo.arn]
+  }
+
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions-haojunsng-strava_pipeline"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+}
+
+resource "aws_iam_policy" "github_actions" {
+  name   = "github-actions"
+  policy = data.aws_iam_policy_document.github_actions.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions.arn
+}
