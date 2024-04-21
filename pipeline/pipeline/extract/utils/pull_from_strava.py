07118e1b30625a24b850977d844acdcd0dc3747a
Sng Hao Jun <51106107+haojunsng@users.noreply.github.com>
2024-04-21 17:15:48 +0800

    SNG-15 feat: Pull historical activities (#27)

--- pipeline/pipeline/extract/utils/pull_from_strava.py
+++ pipeline/pipeline/extract/utils/pull_from_strava.py
@@ -1,12 +1,36 @@
-import requests
-from utils.get_access_token import get_access_token
+from utils.api_methods import get
+import datetime
 
 
 def pull_stats_from_strava(id):
 
+    print(f"Pulling statistics...")
     STATS_ENDPOINT = f"https://www.strava.com/api/v3/athletes/{id}/stats"
+    return get(STATS_ENDPOINT)
 
-    headers = {"Authorization": f"Authorization: Bearer {get_access_token()}"}
-    response = requests.get(STATS_ENDPOINT, headers=headers)
-    response.raise_for_status()
-    return response.json()
+
+def pull_athlete_from_strava():
+
+    print(f"Pulling athlete information...")
+    ATHLETE_ENDPOINT = "https://www.strava.com/api/v3/athlete"
+    return get(ATHLETE_ENDPOINT)
+
+
+def pull_activity_from_strava(all=False, **kwargs):
+
+    if not all:
+        if "start" not in kwargs or "end" not in kwargs:
+            raise ValueError("Missing 'start' and/or 'end' parameters")
+        start = kwargs["before"]
+        end = kwargs["after"]
+
+        print(f"Pulling activities between {start} and {end}")
+        ACTIVITY_ENDPOINT = f"https://www.strava.com/api/v3/athlete/activities?before={end}&after={start}&page=&per_page="
+
+    else:
+        now = int(datetime.datetime.now().timestamp())
+
+        print("Pulling all activities")
+        ACTIVITY_ENDPOINT = f"https://www.strava.com/api/v3/athlete/activities?before={now}&after=0&page=1&per_page=200"
+
+    return get(ACTIVITY_ENDPOINT)
--- pipeline/pipeline/extract/utils/s3_helper.py
+++ pipeline/pipeline/extract/utils/s3_helper.py
@@ -1,10 +1,17 @@
 import boto3
 import json
 import datetime
+from utils.settings import S3_BUCKET_NAME
 
 
-def write_to_s3(data):
-    s3 = boto3.client("s3")
-    S3_BUCKET_NAME = "gomu-landing-bucket"
-    S3_KEY = f'source=strava/type=statistics/date={datetime.datetime.now().strftime("%Y%m%d")}/stats.json'  # TODO: Replace with Airflow ds
-    s3.put_object(Body=json.dumps(data, indent=4), Bucket=S3_BUCKET_NAME, Key=S3_KEY)
+def write_to_s3(data, source_type):
+
+    try:
+        s3 = boto3.client("s3")
+        S3_KEY = f'source=strava/type={source_type}/date={datetime.datetime.now().strftime("%Y%m%d")}/stats.json'  # TODO: Replace with Airflow ds
+        s3.put_object(
+            Body=json.dumps(data, indent=4), Bucket=S3_BUCKET_NAME, Key=S3_KEY
+        )
+    except Exception as e:
+        print("Error writing data to S3:", e)
+        return None
