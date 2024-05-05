8dc64568a94b9dfcf25c92bfc52523a80f2fc8c0
haojunsng <snghaojun18@gmail.com>
2024-05-05 14:56:04 +0800

    SNG-20 feat: Use argparse instead

--- pipeline/pipeline/extract/main.py
+++ pipeline/pipeline/extract/main.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 from utils.s3_helper import write_to_s3
 from utils.pull_from_strava import (
     pull_stats_from_strava,
@@ -9,11 +9,10 @@ from utils.pull_from_strava import (
 
 def main():
 
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <ID>")
-        return
-
-    ID = sys.argv[1]
+    parser = argparse.ArgumentParser(description="Extract STRAVA")
+    parser.add_argument("ID", type=str, help="The ID to process")
+    args = parser.parse_args()
+    ID = args.ID
 
     # Pull snapshot of statistics of athlete - partitioned by `date`
     stats = pull_stats_from_strava(ID)
