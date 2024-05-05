85a52442670502e2c84c058f7dc7ab20b21dd1cb
Sng Hao Jun <51106107+haojunsng@users.noreply.github.com>
2024-05-05 17:34:03 +0800

    SNG-21 feat: Create S3ToSupabaseOperator (#36)
    * SNG-21 feat: Create S3ToSupabaseOperator
    
    * SNG-21 feat: Create S3ToSupabaseOperator
    
    * SNG-21 feat: Add DAG example image
    
    * SNG-21 feat: Add DAG example image

--- pipeline/pipeline/orchestration/dags/utils/settings.py
+++ pipeline/pipeline/orchestration/dags/utils/settings.py
@@ -1,10 +1,26 @@
+import os
+
+
 # Administrative Information
 OWNER = "monkeyDluffy"
 EMAIL = "luffy@imu.com"
 
 # ECS Configurations
-TASK_DEFINITION = "gomu-task-definition"
+GOMU_TASK_DEFINITION = "gomu-task-definition"
+GOMU_CONTAINER_NAME = "gomu-gomu"
+SABO_TASK_DEFINITION = "sabo-task-definition"
+SABO_CONTAINER_NAME = "sabo-sabo"
 ECS_CLUSTER = "gomu-ecs-cluster"
-ECS_CONTAINER_NAME = "gomu-gomu"
-AWSLOGS_GROUP = "gomu-log-group"
-REGION = "ap-southeast-1"
+NETWORK_CONFIGURATION = {
+    "awsvpcConfiguration": {
+        "subnets": [
+            os.environ.get("first_subnet_id"),
+            os.environ.get("second_subnet_id"),
+        ],
+        "securityGroups": [os.environ.get("security_group_id")],
+        "assignPublicIp": "ENABLED",
+    }
+}
+
+# S3
+S3_RAW_BUCKET = "gomu-landing-bucket"
