67c7746470cd44e538ec1471922217f32a7666ff
Sng Hao Jun <51106107+haojunsng@users.noreply.github.com>
2024-07-21 13:51:05 +0800

    chore: Refactor error handling and struct typing (#56)

--- weather/constants.go
+++ weather/constants.go
@@ -5,12 +5,12 @@ const (
 	KafkaTopic       string  = "weather_topic"
 	LatitudePunggol  float32 = 1.3984
 	LongitudePunggol float32 = 103.9072
-	MinTemperature           = -100.0
-	MaxTemperature           = 100.0
-	MinHumidity              = 0.0
-	MaxHumidity              = 100.0
-	MinPressure              = 900.0
-	MaxPressure              = 1100.0
-	MinWindSpeed             = 0.0
-	MaxWindSpeed             = 300.0
+	MinTemperature   float32 = -100.0
+	MaxTemperature   float32 = 100.0
+	MinHumidity      float32 = 0.0
+	MaxHumidity      float32 = 100.0
+	MinPressure      float32 = 900.0
+	MaxPressure      float32 = 1100.0
+	MinWindSpeed     float32 = 0.0
+	MaxWindSpeed     float32 = 300.0
 )
--- weather/main.go
+++ weather/main.go
@@ -3,38 +3,38 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"os"
+	"os/signal"
 	"time"
 )
 
 func main() {
-	producer, err := createProducer()
-	if err != nil {
-		log.Fatalf("Failed to create producer: %v", err)
-	}
+	producer := createProducer()
 	defer producer.Close()
 
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt)
+
 	for {
-		ticker := time.NewTicker(1 * time.Minute)
-		defer ticker.Stop()
+		select {
+		case <-ticker.C:
+			param := createOpenMeteoParams()
+			resp := fetchWeatherData(param)
+			var weatherData WeatherData
 
-		<-ticker.C
-		param := createOpenMeteoParams()
-		resp := fetchWeatherData(param)
-		var weatherData WeatherData
+			// Unmarshal JSON data into struct
+			err := json.Unmarshal([]byte(resp), &weatherData)
+			handleError(err, "Failed to unmarshal JSON into struct")
 
-		// Unmarshal JSON data into struct
-		err := json.Unmarshal([]byte(resp), &weatherData)
-		if err != nil {
-			fmt.Println("Error:", err)
+			message := fmt.Sprintf("Temperature: %.2f, WindSpeed: %.2f",
+				weatherData.CurrentWeather.Temperature, weatherData.CurrentWeather.WindSpeed)
+			produceMessage(producer, KafkaTopic, message)
+		case <-done:
+			fmt.Println("Received interrupt signal, shutting down...")
 			return
-		}
-
-		message := fmt.Sprintf("Temperature: %.2f, WindSpeed: %.2f",
-			weatherData.CurrentWeather.Temperature, weatherData.CurrentWeather.WindSpeed)
-
-		if err := produceMessage(producer, KafkaTopic, message); err != nil {
-			log.Printf("Failed to produce message: %v", err)
 		}
 	}
 }
