5735298967dc84d0a20b0813e148e64acda0c560
haojunsng <snghaojun18@gmail.com>
2024-08-05 10:32:48 +0800

    chore: Restructure/CLAYGO work

--- weather/common/constants.go
+++ weather/common/constants.go
@@ -1,19 +1,32 @@
 package common
 
+// Kafka configuration
 const (
-	KafkaBroker      string  = "localhost:9092"
-	KafkaTopic       string  = "weather_topic"
+	KafkaBroker = "localhost:9092"
+	KafkaTopic  = "weather_topic"
+	GroupID     = "weather_consumer"
+)
+
+// Geographical constants
+const (
 	LatitudePunggol  float32 = 1.3984
 	LongitudePunggol float32 = 103.9072
-	MinTemperature   float32 = -100.0
-	MaxTemperature   float32 = 100.0
-	MinHumidity      float32 = 0.0
-	MaxHumidity      float32 = 100.0
-	MinPressure      float32 = 900.0
-	MaxPressure      float32 = 1100.0
-	MinWindSpeed     float32 = 0.0
-	MaxWindSpeed     float32 = 300.0
-	GroupID          string  = "weather_consumer"
-	S3Bucket         string  = "gomu-landing-bucket"
-	AWSRegion        string  = "ap-southeast-1"
+)
+
+// Weather data ranges
+const (
+	MinTemperature float32 = -100.0
+	MaxTemperature float32 = 100.0
+	MinHumidity    float32 = 0.0
+	MaxHumidity    float32 = 100.0
+	MinPressure    float32 = 900.0
+	MaxPressure    float32 = 1100.0
+	MinWindSpeed   float32 = 0.0
+	MaxWindSpeed   float32 = 300.0
+)
+
+// AWS S3 configuration
+const (
+	S3Bucket = "gomu-landing-bucket"
+	AWSRegion = "ap-southeast-1"
 )
