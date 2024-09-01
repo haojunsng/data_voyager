8234bcf67ec8c2c5a0f79638a8836972f68efe03
haojunsng <snghaojun18@gmail.com>
2024-09-01 15:44:31 +0800

    SNG-38 feat: Provision MSK for weather data

--- iac/logs.tf
+++ iac/logs.tf
@@ -6,3 +6,7 @@ resource "aws_cloudwatch_log_stream" "gomu_log_stream" {
   name           = "gomu-log-stream"
   log_group_name = aws_cloudwatch_log_group.gomu_log_group.name
 }
+
+resource "aws_cloudwatch_log_group" "kafka_log_group" {
+  name = "kafka-broker-logs"
+}
