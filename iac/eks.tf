44d8dffabbd91b724e4feebde1b7d5540132288d
haojunsng <snghaojun18@gmail.com>
2024-09-21 17:24:26 +0800

    SNG-45 fix: Correct irsa role name

--- iac/eks.tf
+++ iac/eks.tf
@@ -71,6 +71,6 @@ resource "aws_iam_role" "irsa_role_kubernetes" {
 }
 
 resource "aws_iam_role_policy_attachment" "attach_irsa_s3_policy" {
-  role       = aws_iam_role.kubernetes_irsa_role.name
+  role       = aws_iam_role.irsa_role_kubernetes.name
   policy_arn = aws_iam_policy.gomu_landing_bucket_policy.arn
 }
