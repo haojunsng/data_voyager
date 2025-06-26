894d77b6ffafbb7eb23011c49488018061ae6302
haojunsng <snghaojun18@gmail.com>
2025-06-26 11:56:55 +0800

    chore: Rename repository

--- iac/github_oidc.tf
+++ iac/github_oidc.tf
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:haojunsng/data_voyager:*"]
+      values   = ["repo:haojunsng/sh-thousand-sunny:*"]
     }
   }
 }
